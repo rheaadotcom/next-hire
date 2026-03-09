2f32d29b265228448da972d90473f3c7d38179ab
Riya Bhardwaj <iriyabhardwaj@gmail.com>
2026-03-09 20:40:33 +0530

    fix env config, inngest email bug, and server setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,39 @@
-// const express=require("express")
- import express from "express";
- import {ENV} from "./lib/env.js";
- const app =express()
+import express from "express";
+import path from "path";
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+import { connectDB } from "../lib/db.js";
 
- console.log(ENV.PORT);
- console.log(ENV.DB_URL);
+const app = express();
+const __dirname = path.resolve();
 
- app.get("/health",(req,res)=>{
-    res.status(200).json({msg:"api is running"});
- })
+//miidlewares
+app.use(express.json())
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
 
- app.listen(ENV.PORT,()=>console.log("Server is running on port:",ENV.PORT));
+app.use("/api/inngest",serve({client:inngest,functions}));
+
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is running" });
+});
+
+// Deployment setup
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
+connectDB().then(() => {
+  app.listen(ENV.PORT, () => {
+    console.log("Server is running on port:", ENV.PORT);
+  });
+});
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
